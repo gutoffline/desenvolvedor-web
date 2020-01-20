54ac46b54f467b2c74314438338d8b481a0f10a8
Guto Xavier <gutoffline@gmail.com>
2020-01-20 20:24:37 -0300

    Jogo advinhação

--- aulas-javascript/aula-03/advinha.js
+++ aulas-javascript/aula-03/advinha.js
@@ -7,6 +7,10 @@ numeroJogador = prompt("Tente adivinhar o número de 0 à 9");
 
 if(numeroComputador == numeroJogador){
     document.write("Você ganhou!");
+    document.write("<br>");
+    document.write("<img src='imagens/feliz.png'>");
 }else{
     document.write("Você perdeu!"); 
+    document.write("<br>");
+    document.write("<img src='imagens/triste.png'>");
 }
