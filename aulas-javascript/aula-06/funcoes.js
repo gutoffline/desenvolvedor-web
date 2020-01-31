fb33d34a2c7185aa1e064a8f372477b95b8f505e
Guto Xavier <gutoffline@gmail.com>
2020-01-30 22:33:42 -0300

    Aula 06

---
+++ aulas-javascript/aula-06/funcoes.js
@@ -0,0 +1,7 @@
+
+
+function MudarSite(){
+    document.getElementById("titulo").innerHTML = "Meu site";
+
+    document.getElementById("login").innerHTML =  "<a href='http://uol.com.br'>UOL</a>";
+}
