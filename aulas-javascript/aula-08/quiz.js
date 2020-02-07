006772c13743328dabfdb03b690d14bc432c47e1
Guto Xavier <gutoffline@gmail.com>
2020-02-06 22:22:42 -0300

    Aula 8

--- aulas-javascript/aula-08/quiz.js
+++ aulas-javascript/aula-08/quiz.js
@@ -1,18 +1,4 @@
-function VerResposta1(){
-    // ALTERNATIVA 1
-    let alternativa1 = document.querySelector("#pergunta1-resposta1").checked;
-    if(alternativa1 == true){
-        document.querySelector(".resposta1").innerHTML = "Certa resposta";
-    }else{
-        document.querySelector(".resposta1").innerHTML = "Resposta errada";
-    }
-}
-
-
-
-
 function VerResposta(questao, respostaCerta){
-    // ALTERNATIVA 1
     let alternativa1 = document.querySelector("#pergunta"+questao+"-resposta"+respostaCerta).checked;
     if(alternativa1 == true){
         document.querySelector(".resposta" + questao).innerHTML = "Certa resposta";
