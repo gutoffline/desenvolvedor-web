45fb9eff780ceedeba2c099d031c1d2f25e3dfef
Guto Xavier <gutoffline@gmail.com>
2020-02-20 22:22:37 -0300

    Aula 12 JS

--- aulas-javascript/aula-12/cursos.js
+++ aulas-javascript/aula-12/cursos.js
@@ -1,10 +1,4 @@
 function Expandir(elemento){
-    let classeAtual = document.querySelector(elemento).className;
-
-    if(classeAtual == 'aberto'){
-        document.querySelector(elemento).className = "fechado";
-    }else{
-        document.querySelector(elemento).className = "aberto";
-    }
-
+    document.querySelector('.aberto').className = 'fechado';
+    document.querySelector(elemento).className = "aberto";
 }
